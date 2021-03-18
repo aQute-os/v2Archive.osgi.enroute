6ac27afdf5b330d7726a3ac00398400c8f1a491d
Peter Kriens <Peter.Kriens@aqute.biz>
2021-03-18 19:17:30 +0100

    [root] Access to the root
    It turned out that the JS code for the hgui made
    the assumption that the root / was served as actual
    content. The web server unfortunately did a 
    redirect to the actual file.
    
    However, JS is nowadays quite brain damaged and
    gets a tad lost when the root url becomes /index.html.
    
    This patch allows a bundle to set Web-Root manifest
    header to a file. If any bundle has this header,
    it will be served for '/'
    
    If there are multiple bundles with this header,
    a warning is logged once and then one of them is 
    chosen.

--- osgi.enroute.web.simple.provider/src/osgi/enroute/web/server/provider/RedirectServlet.java
+++ osgi.enroute.web.simple.provider/src/osgi/enroute/web/server/provider/RedirectServlet.java
@@ -1,15 +1,23 @@
 package osgi.enroute.web.server.provider;
 
-import java.util.Map;
+import java.net.URL;
+import java.util.Collection;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.BundleEvent;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.ConfigurationPolicy;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.util.tracker.BundleTracker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import aQute.lib.io.IO;
 import osgi.enroute.servlet.api.ConditionalServlet;
 import osgi.enroute.web.server.exceptions.Redirect302Exception;
 
@@ -19,20 +27,36 @@ import osgi.enroute.web.server.exceptions.Redirect302Exception;
 	"service.ranking:Integer=1000", "name=" + RedirectServlet.NAME,
 }, name = RedirectServlet.NAME, configurationPolicy = ConfigurationPolicy.OPTIONAL)
 public class RedirectServlet implements ConditionalServlet {
-
-	static final String	NAME		= "osgi.enroute.simple.redirect";
+	final static Logger			logger			= LoggerFactory.getLogger(RedirectServlet.class);
+	static final String			NAME			= "osgi.enroute.simple.redirect";
 
 	/**
 	 * Must start with a "/".
 	 */
-	private String		redirect	= "/index.html";
+	private String				redirect		= "/index.html";
+
+	final BundleTracker<URL>	bundles;
+	boolean						neverReported	= true;
 
 	@interface Config {
 		String redirect();
 	}
 
 	@Activate
-	void activate(Config config, Map<String, Object> props, BundleContext context) throws Exception {
+	public RedirectServlet(Config config, BundleContext context) throws Exception {
+		bundles = new BundleTracker<URL>(context, Bundle.ACTIVE + Bundle.STARTING, null) {
+			@Override
+			public URL addingBundle(Bundle bundle, BundleEvent event) {
+				String root = bundle.getHeaders()
+					.get("Web-Root");
+				if (root == null)
+					return null;
+
+				return bundle.getResource(root);
+			}
+		};
+		bundles.open();
+
 		if (config.redirect() != null)
 			redirect = config.redirect();
 
@@ -40,8 +64,32 @@ public class RedirectServlet implements ConditionalServlet {
 			redirect = "/" + redirect;
 	}
 
+	@Deactivate
+	void close() {
+		bundles.close();
+	}
+
 	@Override
 	public boolean doConditionalService(HttpServletRequest rq, HttpServletResponse rsp) throws Exception {
+		String path = rq.getRequestURI();
+
+		Collection<URL> tracked = bundles.getTracked()
+			.values();
+		if (path != null && path.equals("/") && !tracked.isEmpty()) {
+
+			if (tracked.size() > 1 && neverReported) {
+				logger.warn("There are multiple web roots defined {}", tracked);
+				neverReported = false;
+			}
+
+			URL first = tracked.iterator()
+				.next();
+
+			IO.copy(first.openStream(), rsp.getOutputStream());
+			rsp.setStatus(200);
+			return true;
+		}
+
 		// Redirect is disabled by configuring with an empty string.
 		// Since the value will be prepended with "/", it means that when the
 		// value is "/", no action is taken.
@@ -49,7 +97,6 @@ public class RedirectServlet implements ConditionalServlet {
 			return false;
 
 		try {
-			String path = rq.getRequestURI();
 
 			if (path == null || path.isEmpty() || path.equals("/")) {
 				throw new Redirect302Exception(redirect);
@@ -68,4 +115,5 @@ public class RedirectServlet implements ConditionalServlet {
 			return true;
 		}
 	}
+
 }
