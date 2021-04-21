cff999858bcff772edaefcc6bff47a77eeae1379
Peter Kriens <Peter.Kriens@aqute.biz>
2021-04-21 08:29:02 +0200

    DTOs

--- osgi.enroute.base.api/src/osgi/enroute/dto/api/PrimaryKey.java
+++ osgi.enroute.base.api/src/osgi/enroute/dto/api/PrimaryKey.java
@@ -1,6 +1,13 @@
 package osgi.enroute.dto.api;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * A primary key annotation
  */
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
 public @interface PrimaryKey {}
